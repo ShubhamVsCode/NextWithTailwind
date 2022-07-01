c4d9d8df303c9a4fd2a30e43c71d937ba2917a4a
ShubhamVsCode <shubhamvscode@gmail.com>
2022-07-01 14:08:55 +0530

    title changed and minor change in footer

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <div className="">
+      <Head>
+        <title>Online Eduction| Null | Shubham</title>
+      </Head>
       <div className="heroBg">
         <section className="py-10 mx-auto max-w-7xl">
           <Navbar />
